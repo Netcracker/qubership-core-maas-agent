7a4ea4812f33244d2c8d0c4552e768842b9d0212
Evgenii Stetsenko <i@estetsenko.com>
2026-01-22 15:44:15 +0300

    fix: make helm deployable (#106)

--- maas-agent-service/config/config.go
+++ maas-agent-service/config/config.go
@@ -1,10 +1,12 @@
 package config
 
 import (
+	fiberSec "github.com/netcracker/qubership-core-lib-go-fiber-server-utils/v2/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/serviceloader"
 )
 
 func init() {
 	serviceloader.Register(1, &security.DummyToken{})
+	serviceloader.Register(1, &fiberSec.DummyFiberServerSecurityMiddleware{})
 }
