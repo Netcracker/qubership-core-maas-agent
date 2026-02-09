d857c3d2de3ace87db17980ca9bad5c6488760cd
alsergs <sergei.s.aleksandrov.qubership@gmail.com>
2026-02-09 14:35:08 +0300

    feat: memlimit

--- maas-agent-service/server.go
+++ maas-agent-service/server.go
@@ -3,6 +3,9 @@ package main
 import (
 	_ "github.com/netcracker/qubership-core-maas-agent/maas-agent-service/v2/config"
 	service "github.com/netcracker/qubership-core-maas-agent/maas-agent-service/v2/lib"
+
+	// memlimit sets memory limit = 0.9 of cgroup memory limit
+	_ "github.com/netcracker/qubership-core-lib-go/v3/memlimit"
 )
 
 func main() {
